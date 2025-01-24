f9d05b4f05496340c7b73f162e5ad9ff409dac96
sanish11 <59250164+sanish11@users.noreply.github.com>
2025-01-24 15:08:15 +0545

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,23 @@
 node(){
-    environment{
-        DIR = 'unzipped'
-    }
-    echo "Workspace cleaned. Checking contents:"
-   
+    deleteDir()
     properties([
         parameters([
-            base64File(name: 'FILE', description: 'Upload a file'),
+            // base64File(name: 'FILE', description: 'Upload a file'),
             string(name: 'Language', defaultValue: 'es', description: 'Country code')
             
         ])
     ])
+    
+    stage('Clone and Execute Script') {
+        echo "Cloning Python script from GitHub..."
+        sh "git clone https://github.com/sanish11/python-CQ.git . "
+        
+    }
+    
     withFileParameter('FILE') {
-    stage("Unzip"){
+    stage("ada"){
         sh """
-            unzip -o "${FILE}" -d "${DIR}"
+            unzip -o "${FILE}" -d "unzipped"
         """
         }
     }
