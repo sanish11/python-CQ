a760ed7736661144905aa42bf0029787519c6c7b
sanish11 <59250164+sanish11@users.noreply.github.com>
2025-01-24 15:56:38 +0545

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,24 +2,43 @@ node(){
     deleteDir()
     properties([
         parameters([
-            // base64File(name: 'FILE', description: 'Upload a file'),
+            base64File( description:'upload a file', name: 'FILE'),
             string(name: 'Language', defaultValue: 'es', description: 'Country code')
             
         ])
     ])
     
+
+    
+    withFileParameter('FILE') {
+        
     stage('Clone and Execute Script') {
         echo "Cloning Python script from GitHub..."
+        
         sh "git clone https://github.com/sanish11/python-CQ.git . "
         
     }
-    
-    withFileParameter('FILE') {
     stage("ada"){
         sh """
             unzip -o "${FILE}" -d "unzipped"
         """
         }
+    
+    
+    
+    stage("Python Script"){
+        echo "ada"
+        def stfFile = sh(script: 'find unzipped/ -name "*.stf" -print -quit', returnStdout: true).trim()
+        sh """
+        
+            python3 stf.py "${stfFile}" output.stf "${params.Language}"
+        """
+    }
+    
+    stage('Archive Artifact') {
+        echo "Archiving output_file.stf..."
+        archiveArtifacts artifacts: 'output.stf', allowEmptyArchive: true
+    }
     }
 
 }
