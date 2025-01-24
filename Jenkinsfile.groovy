5fc6a59990e41c9e9e8a340ff5bb6c62eea23867
sanish11 <59250164+sanish11@users.noreply.github.com>
2025-01-24 14:42:27 +0545

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+node(){
+    deleteDir()
+    echo "Workspace cleaned. Checking contents:"
+    sh "ls -la"
+    properties([
+        parameters([
+            base64File(name: 'FILE', description: 'Upload a file'),
+            string(name: 'Language', defaultValue: 'es', description: 'Country code')
+            
+        ])
+    ])
+    withFileParameter('FILE') {
+    stage("Unzip"){
+        sh """
+            unzip -o "${FILE}" -d "${WORKSPACE}/unzipped"
+        """
+        }
+    }
+
+}
